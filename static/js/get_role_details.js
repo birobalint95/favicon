7c991eafee67cc6387130e4e9196c8c262367747
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-09-17 18:18:09 +0200

    selected class added to json parsing

--- static/js/get_role_details.js
+++ static/js/get_role_details.js
@@ -5,17 +5,23 @@ fetch("./static/json/role_details.json")
 .then(function(roles){
     let placeholder = document.querySelector("#list-of-roles");
     let out = "";
+    var indexOfRole = 1;
+    var role_class = "rb-item"
     for(let role of roles){
+        if (indexOfRole = 1) {
+            role_class += " selected"
+        }
         out += `
-            <li class="rb-item" ng-repeat="itembx">
+            <li class=${role_class} ng-repeat="itembx">
                 <a id=${role.id} href="javascript:void(0)" onclick="selectPosition(this.id)">
                     <div class="item-title">${role.title}</div>
                     <div class="timestamp">
-                        ${role.company}<br> ${role.area}
+                        ${role.company}<br>${role.area}
                     </div>
                 </a>
             </li>
         `;
+        indexOfRole++
    }
    placeholder.innerHTML = out;
 });
