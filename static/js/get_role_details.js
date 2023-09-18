1558df84367c5760f1b360b6b6ecd4ed060a9c5f
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-09-18 20:45:57 +0200

    images and role details added

--- static/js/get_role_details.js
+++ static/js/get_role_details.js
@@ -40,7 +40,21 @@ fetch("./static/json/role_details.json")
                     <h4 class="company-title">${role.company}, ${role.area}</h4>
                 </div>
                 <div class="content-section">
-                    ${role.content}
+                    <group id="group-of-details">
+                        <a id="${role.id}-responsibilities" class="detail-category" href="#" onclick="selectRoleDetail(this.id)">Responsibilities</a>
+                        <a id="${role.id}-skills" class="detail-category" href="#" onclick="selectRoleDetail(this.id)">Skills</a>
+                        <a id="${role.id}-tools" class="detail-category" href="#" onclick="selectRoleDetail(this.id)">Tools</a>
+                    </group>
+                    <div id="tab-content-responsibilities" title="${role.id}-responsibilities" class="tab-content">
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?</p>
+                        <p>Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?</p>
+                    </div>
+                    <div id="tab-content-skills" title="${role.id}-skills" class="tab-content">
+                        <p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?</p>
+                    </div>
+                    <div id="tab-content-tools" title="${role.id}-tools" class="tab-content">
+                        <p>Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?</p>
+                    </div>
                 </div>
             </div>
         `;
---
+++ static/js/select_role_detail.js
@@ -0,0 +1,25 @@
+function selectRoleDetail(clicked_id) {
+    var clickedElement = document.getElementById(clicked_id);
+  var isElementSelected = clickedElement.classList.contains("selected");
+  var allDetailSelectorItems = document.getElementsByClassName("detail-category");
+  
+      if (! isElementSelected) {
+        [].forEach.call(allDetailSelectorItems, function(actualDetailSelectorItem) {
+            actualDetailSelectorItem.classList.remove("selected");
+        });
+    clickedElement.classList.add("selected");
+    };
+  
+      var detailItemToLookFor = document.querySelector("[title=" + CSS.escape(clicked_id) + "]");
+  
+    var areDetailsDisplayed = detailItemToLookFor.classList.contains("displayed");
+  var allDetailItems = document.getElementsByClassName("tab-content");
+ 
+      if (! areDetailsDisplayed) {
+        [].forEach.call(allDetailItems, function(actualDetailItem){
+            actualDetailItem.classList.remove("displayed");
+        });
+ detailItemToLookFor.classList.add("displayed");
+    };
+  
+};
