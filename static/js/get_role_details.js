07f9e8912f42f1d26f10c42e4c629aeb15aa4b3f
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-09-20 11:19:10 +0200

    role detail tab selection updated

--- static/js/get_role_details.js
+++ static/js/get_role_details.js
@@ -41,9 +41,9 @@ fetch("./static/json/role_details.json")
                 </div>
                 <div class="content-section">
                     <group id="group-of-details">
-                        <a id="${role.id}-responsibilities" class="detail-category selected" href="#" onclick="selectRoleDetail(this.id)">Responsibilities</a>
-                        <a id="${role.id}-skills" class="detail-category" href="#" onclick="selectRoleDetail(this.id)">Skills</a>
-                        <a id="${role.id}-tools" class="detail-category" href="#" onclick="selectRoleDetail(this.id)">Tools</a>
+                        <a id="${role.id}-responsibilities" class="detail-category selected" href="#" onclick="selectRoleDetailTab(this.id)">Responsibilities</a>
+                        <a id="${role.id}-skills" class="detail-category" href="#" onclick="selectRoleDetailTab(this.id)">Skills</a>
+                        <a id="${role.id}-tools" class="detail-category" href="#" onclick="selectRoleDetailTab(this.id)">Tools</a>
                     </group>
                     <div id="tab-content-responsibilities" title="${role.id}-responsibilities" class="tab-content displayed">
                         <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?</p>
--- static/js/select_role_detail.js
+++ static/js/select_role_detail.js
@@ -29,14 +29,15 @@ function selectRoleDetail(clicked_id) {
 };
 
 function selectRoleDetailTab(roleDetailSelectorId){
-    var roleDetailSelectorId = "product-owner-2-responsibilities";
-
     var allRoleDetailSelectorItems = document.getElementsByClassName("detail-category");
     [].forEach.call(allRoleDetailSelectorItems, function(actualRoleDetailSelectorItem) {
+        var actualRoleDetailContentItem = document.querySelector("[title=" + CSS.escape(actualRoleDetailSelectorItem.id) + "]");
         if(actualRoleDetailSelectorItem.id == roleDetailSelectorId){
             actualRoleDetailSelectorItem.classList.add("selected");
+            actualRoleDetailContentItem.classList.add("displayed");
         } else {
             actualRoleDetailSelectorItem.classList.remove("selected");
+            actualRoleDetailContentItem.classList.remove("displayed");
         }
     });
 };
