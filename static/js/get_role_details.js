e0ca807c4c9a0912225ce18102716ab0a65e8d1d
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-09-20 14:15:53 +0200

    skills from json on webpage

--- static/js/get_role_details.js
+++ static/js/get_role_details.js
@@ -33,12 +33,10 @@ fetch("./static/json/role_details.json")
         `;
 
         for(let skill of role.skills){
-            console.log(skill.name);
-            console.log(skill.description);
             skillsOfRoleHtml += `
                 <div>
-                    <span class="role-skill">${skill.key}</span>
-                    <p>${skill.value}</p>
+                    <span class="role-skill">${skill.name}</span>
+                    <p>${skill.description}</p>
                 </div>
             `
         };
@@ -62,10 +60,7 @@ fetch("./static/json/role_details.json")
                         <p>Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?</p>
                     </div>
                     <div id="tab-content-${role.id}-skills" title="${role.id}-skills" class="tab-content">
-                        <div>
-                            <span class="role-skill">Scaled Agile Framework</span>
-                            <p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?</p>
-                        </div>
+                        ${skillsOfRoleHtml}
                     </div>
                     <div id="tab-content-${role.id}-tools" title="${role.id}-tools" class="tab-content">
                         <p>Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?</p>
