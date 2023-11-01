3e8e1caf1e55fdd353841d6f29fe26122f9c1953
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-11-01 21:27:45 +0100

    skill related js files updated

--- static/js/get_skills.js
+++ static/js/get_skills.js
@@ -16,6 +16,25 @@ fetch("./static/json/role_details.json")
         }
     }
 
-    console.log(dictOfSkills);
+    for (const [skill, roles] of Object.entries(dictOfSkills)) {
+        console.log(skill, roles);
+    }
+
+
+    detailsItemsOfSkills += `
+        <details class="about-skill-details skill-card" style="display: flex;">
+            <summary class="about-skill-name">
+                <span class="about-skill-description" style="font-weight: bold;">Scaled Agile Framework</span>
+                <p style="font-size: 80%;" class="about-role-count">in <span class="number-of-roles">3</span> roles</p>
+            </summary>
+            <ul class="skill-description" style="padding-left: 90px; font-size: 80%; padding-top: 0px;">
+                <li>Test Automation Product Owner</li>
+                <li>Automation & Framework Product Owner</li>
+                <li>Chief Engineer</li>
+            </ul>
+        </details>
+    `;
 
 });
+
+
--- static/js/open_skill_details.js
+++ static/js/open_skill_details.js
@@ -3,17 +3,16 @@ $(document).ready (function() {
     var allDetails = document.querySelectorAll(".about-skill-details")
 
     allDetails.forEach(details => {
-    details.addEventListener("toggle", function() {
-        isDetailsOpen = details.hasAttribute("open");
-        numberOfRoles = details.getElementsByClassName("number-of-roles")[0];
-        numberOfRolesInt = parseInt(numberOfRoles.innerText);
-        if (isDetailsOpen) {
-            requiredHeight = 90 + (numberOfRolesInt - 1)*27;
-            details.style.height = requiredHeight.toString() + "px";
-        } else {
-            details.style.height = "70px";
-        }
-    })
+        details.addEventListener("toggle", function() {
+            isDetailsOpen = details.hasAttribute("open");
+            numberOfRoles = details.getElementsByClassName("number-of-roles")[0];
+            numberOfRolesInt = parseInt(numberOfRoles.innerText);
+            if (isDetailsOpen) {
+                requiredHeight = 90 + (numberOfRolesInt - 1)*27;
+                details.style.height = requiredHeight.toString() + "px";
+            } else {
+                details.style.height = "70px";
+            }
+        })
     });
-
 });
