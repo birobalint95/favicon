a6fb1f646234e93c41e79be22b6d502e4b403a16
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-11-01 15:01:53 +0100

    get span text

--- static/js/open_skill_details.js
+++ static/js/open_skill_details.js
@@ -5,7 +5,8 @@ $(document).ready (function() {
     allDetails.forEach(details => {
     details.addEventListener("toggle", function() {
         isDetailsOpen = details.hasAttribute("open");
-        numberOfRoles = details.getElementsByClassName("number-of-roles");
+        numberOfRoles = details.getElementsByClassName("number-of-roles")[0];
+        console.log(numberOfRoles.innerText);
         console.log(numberOfRoles.innerHTML);
         console.log(numberOfRoles.value);
         if (isDetailsOpen) {
