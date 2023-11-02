6969f38493823cc752f384bc655391b305cf5711
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-11-02 18:00:17 +0100

    try 100

--- static/js/create_certification_cards.js
+++ static/js/create_certification_cards.js
@@ -1,39 +1,42 @@
 document.body.onload = createCertificationCards;
 
 function createCertificationCards(){
-    fetch("./static/json/intro_details.json")
-    .then(function(response){
-    return response.json();
-    })
-    .then(function(introDetails){    
 
-        let jsonDataOfCertifications = introDetails["certifications"];
+    console.log("HELLO");
 
-        for(let certification of jsonDataOfCertifications){
-            // ANCHOR ELEMENT
-            const certificationCard = document.createElement("a");
-            certificationCard.setAttribute("class", "certification-card");
-            certificationCard.setAttribute("href", `${certification.pdf_location}`);
+    // fetch("./static/json/intro_details.json")
+    // .then(function(response){
+    // return response.json();
+    // })
+    // .then(function(introDetails){    
 
-            // IMAGE OF ANCHOR
-            const imageOfCertificationCard = document.createElement("img");
-            imageOfCertificationCard.setAttribute("src", `${certification.image_location}`);
-            certificationCard.appendChild(imageOfCertificationCard);
+    //     let jsonDataOfCertifications = introDetails["certifications"];
 
-            // DIV OF ANCHOR
-            const divOfCertificationCard = document.createElement("div");
-            divOfCertificationCard.setAttribute("class", "certification-card-content");
+    //     for(let certification of jsonDataOfCertifications){
+    //         // ANCHOR ELEMENT
+    //         const certificationCard = document.createElement("a");
+    //         certificationCard.setAttribute("class", "certification-card");
+    //         certificationCard.setAttribute("href", `${certification.pdf_location}`);
 
-            // PARAGRAPH OF DIV
-            const paragraphOfDiv = document.createElement("p");
-            paragraphOfDiv.insertAdjacentHTML("beforeend", `${certification.name}`)
-            divOfCertificationCard.appendChild(paragraphOfDiv);
-            certificationCard.appendChild(divOfCertificationCard);
+    //         // IMAGE OF ANCHOR
+    //         const imageOfCertificationCard = document.createElement("img");
+    //         imageOfCertificationCard.setAttribute("src", `${certification.image_location}`);
+    //         certificationCard.appendChild(imageOfCertificationCard);
 
-            // ADD CERTIFICATION CARD TO ITS SECTION
-            const certificationCardSection = document.getElementById("certification-cards-section");
-            certificationCardSection.appendChild(certificationCard);
+    //         // DIV OF ANCHOR
+    //         const divOfCertificationCard = document.createElement("div");
+    //         divOfCertificationCard.setAttribute("class", "certification-card-content");
 
-        }
-    });
+    //         // PARAGRAPH OF DIV
+    //         const paragraphOfDiv = document.createElement("p");
+    //         paragraphOfDiv.insertAdjacentHTML("beforeend", `${certification.name}`)
+    //         divOfCertificationCard.appendChild(paragraphOfDiv);
+    //         certificationCard.appendChild(divOfCertificationCard);
+
+    //         // ADD CERTIFICATION CARD TO ITS SECTION
+    //         const certificationCardSection = document.getElementById("certification-cards-section");
+    //         certificationCardSection.appendChild(certificationCard);
+
+    //     }
+    // });
 };
