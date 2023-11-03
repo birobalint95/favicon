6998ccc01a62916bc16dc56eedc019a48f17aa8b
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-11-03 13:28:53 +0100

    log roles json data

--- static/js/populate_about_page.js
+++ static/js/populate_about_page.js
@@ -1,9 +1,23 @@
-import { createSkillCards } from "./create_skill_cards.js";
-import { createCertificationCards } from "./create_certification_cards.js";
-import { createHobbyCards } from "./create_hobby_cards.js";
+// import { createSkillCards } from "./create_skill_cards.js";
+// import { createCertificationCards } from "./create_certification_cards.js";
+// import { createHobbyCards } from "./create_hobby_cards.js";
 
 $(document).ready(function(){
-    createSkillCards();
-    createCertificationCards();
-    createHobbyCards();
+
+    let rolesJsonData;
+
+    fetch("./static/json/role_details.json")
+    .then(function(response){
+    return response.json();
+    })
+    .then(function(roles){ 
+        rolesJsonData = roles;
+    });
+
+    console.log(rolesJsonData);
+
+
+    // createSkillCards();
+    // createCertificationCards();
+    // createHobbyCards();
 });
