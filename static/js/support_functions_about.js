eec0e78332bfe808756cfdf86b124025aefa3adc
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-11-06 13:46:45 +0100

    last child check

--- static/js/support_functions_about.js
+++ static/js/support_functions_about.js
@@ -25,7 +25,7 @@ function getStudyDetails(clicked_id) {
 function selectAboutDetailTab(aboutDetailSelector){
     let aboutDetailSelectorId = aboutDetailSelector.id;
     console.log($(aboutDetailSelector).index());
-    console.log(aboutDetailSelector === aboutDetailSelector.parentNode.lastChild);
+    console.log($(aboutDetailSelector.parentNode.lastChild).index());
     var allRoleDetailSelectorItems = document.getElementsByClassName("detail-category");
     [].forEach.call(allRoleDetailSelectorItems, function(actualRoleDetailSelectorItem) {
         var actualRoleDetailContentItem = document.querySelector("[title=" + CSS.escape(actualRoleDetailSelectorItem.id) + "]");
