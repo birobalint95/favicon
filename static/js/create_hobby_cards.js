4d8e7fbe09909d12dec4e205df48d58e8f586778
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-11-09 13:46:13 +0100

    generic card creation function

--- static/js/create_hobby_cards.js
+++ static/js/create_hobby_cards.js
@@ -1,31 +1,29 @@
 export {createHobbyCards};
 
 function createHobbyCards(introJsonData){ 
-
     let jsonDataOfHobbies = introJsonData["hobbies"];
-
     for(let hobby of jsonDataOfHobbies){
-        // CARD
-        const hobbyCard = document.createElement("div");
-        hobbyCard.setAttribute("class", "generic-card-with-image");
-        // hobbyCard.setAttribute("style", "display: flex;");
-
-        // CARD IMAGE
-        const imgOfHobbyCard = document.createElement("img");
-        imgOfHobbyCard.setAttribute("class", "image-of-generic-card");
-        imgOfHobbyCard.setAttribute("src", `${hobby.image_location}`);
-        // imgOfHobbyCard.setAttribute("style", "max-width: 100%; max-height: 100%; display: block; border-radius: 10px;");
-        hobbyCard.appendChild(imgOfHobbyCard)
-
-        // CARD TEXT
-        const div2OfHobbyCard = document.createElement("div");
-        div2OfHobbyCard.setAttribute("class", "content-of-generic-card");
-        div2OfHobbyCard.innerText = `${hobby.name}`;
-        hobbyCard.appendChild(div2OfHobbyCard);
-
+        // CREATE CARD
+        let hobbyCard = createGenericCardWithImage(hobby.name, hobby.image_location);
         // ADD CARD TO ITS SECTION
         const hobbyCardSection = document.getElementById("hobby-cards-section");
         hobbyCardSection.appendChild(hobbyCard);
-
     }
 };
+
+function createGenericCardWithImage(text, image_location){
+    // CARD ELEMENT
+    const genericCardWithImage = document.createElement("div");
+    genericCardWithImage.setAttribute("class", "generic-card-with-image");
+    // IMAGE OF CARD ELEMENT
+    const imgOfGenericCard = document.createElement("img");
+    imgOfGenericCard.setAttribute("class", "image-of-generic-card");
+    imgOfGenericCard.setAttribute("src", `${image_location}`);
+    genericCardWithImage.appendChild(imgOfGenericCard)
+    // TEXT OF CARD ELEMENT
+    const contentOfGenericCard = document.createElement("div");
+    contentOfGenericCard.setAttribute("class", "content-of-generic-card");
+    contentOfGenericCard.innerText = `${text}`;
+    genericCardWithImage.appendChild(contentOfGenericCard);
+    return genericCardWithImage
+}
