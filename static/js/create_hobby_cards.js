881c263761df7e83248f73087eb6d76e2da18d31
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-11-09 13:37:58 +0100

    generic card

--- static/js/create_hobby_cards.js
+++ static/js/create_hobby_cards.js
@@ -5,41 +5,27 @@ function createHobbyCards(introJsonData){
     let jsonDataOfHobbies = introJsonData["hobbies"];
 
     for(let hobby of jsonDataOfHobbies){
-        // DIV ELEMENT
+        // CARD
         const hobbyCard = document.createElement("div");
-        hobbyCard.setAttribute("class", "hobby-card");
-        hobbyCard.setAttribute("style", "display: flex;");
+        hobbyCard.setAttribute("class", "generic-card-with-image");
+        // hobbyCard.setAttribute("style", "display: flex;");
 
-        // DIV 1 OF MAIN DIV
-        // const div1OfHobbyCard = document.createElement("div");
+        // CARD IMAGE
         const imgOfHobbyCard = document.createElement("img");
-        imgOfHobbyCard.setAttribute("class", "hobby-icon");
+        imgOfHobbyCard.setAttribute("class", "image-of-generic-card");
         imgOfHobbyCard.setAttribute("src", `${hobby.image_location}`);
-        imgOfHobbyCard.setAttribute("style", "max-width: 100%; max-height: 100%; display: block; border-radius: 10px;");
-        // div1OfHobbyCard.appendChild(imgOfHobbyCard);
-        // hobbyCard.appendChild(div1OfHobbyCard);
+        // imgOfHobbyCard.setAttribute("style", "max-width: 100%; max-height: 100%; display: block; border-radius: 10px;");
         hobbyCard.appendChild(imgOfHobbyCard)
 
-        // DIV 2 OF MAIN DIV
+        // CARD TEXT
         const div2OfHobbyCard = document.createElement("div");
-        div2OfHobbyCard.setAttribute("class", "hobby-card-content");
-
-        // PARAGRAPH OF DIV 2
-        // const paragraphOfDiv2 = document.createElement("p");
-        // paragraphOfDiv2.setAttribute("class", "hobby-description");
-        // paragraphOfDiv2.insertAdjacentHTML("beforeend", `${hobby.name}`)
-        // div2OfHobbyCard.appendChild(paragraphOfDiv2);
+        div2OfHobbyCard.setAttribute("class", "content-of-generic-card");
         div2OfHobbyCard.innerText = `${hobby.name}`;
         hobbyCard.appendChild(div2OfHobbyCard);
 
-        // ADD HOBBY CARD TO ITS SECTION
+        // ADD CARD TO ITS SECTION
         const hobbyCardSection = document.getElementById("hobby-cards-section");
         hobbyCardSection.appendChild(hobbyCard);
 
     }
 };
-
-{/* <div class="hobby-card" style="display: flex;">
-    <img class="hobby-icon" src="./static/images/hobby_hiking.svg" style="max-width: 100%; max-height: 100%; display: block; border-radius: 10px;"/>
-    <div class="hobby-card-content">Hiking</div>
-</div> */}
