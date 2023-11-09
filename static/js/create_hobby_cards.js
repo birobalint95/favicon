6fde813db0e4e260b29747878b8faa30fb94ace2
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2023-11-09 13:52:48 +0100

    support functions extended

--- static/js/create_hobby_cards.js
+++ static/js/create_hobby_cards.js
@@ -10,20 +10,3 @@ function createHobbyCards(introJsonData){
         hobbyCardSection.appendChild(hobbyCard);
     }
 };
-
-function createGenericCardWithImage(text, image_location){
-    // CARD ELEMENT
-    const genericCardWithImage = document.createElement("div");
-    genericCardWithImage.setAttribute("class", "generic-card-with-image");
-    // IMAGE OF CARD ELEMENT
-    const imgOfGenericCard = document.createElement("img");
-    imgOfGenericCard.setAttribute("class", "image-of-generic-card");
-    imgOfGenericCard.setAttribute("src", `${image_location}`);
-    genericCardWithImage.appendChild(imgOfGenericCard)
-    // TEXT OF CARD ELEMENT
-    const textOfGenericCard = document.createElement("span");
-    textOfGenericCard.setAttribute("class", "content-of-generic-card");
-    textOfGenericCard.innerText = `${text}`;
-    genericCardWithImage.appendChild(textOfGenericCard);
-    return genericCardWithImage
-}
